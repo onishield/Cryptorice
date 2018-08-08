aabad4ffc1ae767b9aa8144b46da620231f129c5
BANKKII'S <onishield@gmail.com>
2018-08-08 13:38:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -87,14 +87,14 @@ function cipher(msg){
 }
 
 function cut(msg){
-    var temp = msg
+    var temp = ''
     for (var i = 0; i < 12; i++) {
     //  var index = Math.floor(Math.random() * temp.length - i)
     //  var item = temp.splice(index,1);
     //  temp.push(item);
       var temp += i
     }
-    var newmsg = msg + ', msg.length:' + temp.length + ', iter count:' + temp
+    var newmsg = msg + ', msg.length:' + msg.length + ', iter count:' + temp
     return newmsg
 }
 
